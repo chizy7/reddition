602636d909e6381b8b2144dd81905289ff5c9e06
Chizaram Chibueze <chizy7@gmail.com>
2022-11-13 15:33:10 -0500

    More signIn features for Google and const models

--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -1,10 +1,10 @@
-//import 'dart:html';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:reddit_clone/core/constants/constants.dart';
 import 'package:reddit_clone/core/providers/firebase_providers.dart';
+import 'package:reddit_clone/models/user_model.dart';
 
 final authRepositoryProvider = Provider(
   (ref) => AuthRepository(
@@ -27,6 +27,8 @@ class AuthRepository {
         _firestore = firestore,
         _googleSignIn = googleSignIn;
 
+  CollectionReference get _users => _firestore.collection('users');
+
   void signInWithGoogle() async {
     try {
       final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
@@ -41,7 +43,15 @@ class AuthRepository {
       UserCredential userCredential =
           await _auth.signInWithCredential(credential);
 
-      print(userCredential.user?.email);
+      UserModel userModel = UserModel(
+        name: userCredential.user!.displayName ?? 'No Name',
+        profilePic: userCredential.user!.photoURL ?? Constants.avatarDefault,
+        banner: Constants.bannerDefault,
+        uid: userCredential.user!.uid,
+        isAuthenticated: true,
+        karma: 0,
+        awards: [],
+      );
     } catch (e) {
       print(e);
     }

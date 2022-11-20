fe12ce1339fa4e0165173a87beb213bd27795a0d
Chizaram Chibueze <chizy7@gmail.com>
2022-11-20 13:39:12 -0500

    community drawer bug fixed

--- lib/features/auth/home/drawers/community_list_drawer.dart
+++ lib/features/auth/home/drawers/community_list_drawer.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_clone/core/common/error_text.dart';
+import 'package:reddit_clone/features/auth/controller/auth_controller.dart';
 import 'package:reddit_clone/features/community/controller/community_controller.dart';
+import 'package:reddit_clone/models/community_model.dart';
 import 'package:routemaster/routemaster.dart';
 
 import '../../../../core/common/loader.dart';
+import '../../../../core/common/sign_in_button.dart';
 
 class CommunityListDrawer extends ConsumerWidget {
   const CommunityListDrawer({super.key});
@@ -13,38 +16,90 @@ class CommunityListDrawer extends ConsumerWidget {
     Routemaster.of(context).push('/create-community');
   }
 
+  void navigateToCommunity(BuildContext context, Community community) {
+    Routemaster.of(context).push('/r/${community.name}');
+  }
+
+  // @override
+  // Widget build(BuildContext context, WidgetRef ref) {
+  //   final user = ref.watch(userProvider)!;
+  //   final isGuest = !user.isAuthenticated;
+  //   return Drawer(
+  //     child: SafeArea(
+  //       child: Column(
+  //         children: [
+  //           ListTile(
+  //             title: const Text('Create a Community'),
+  //             leading: const Icon(Icons.add),
+  //             onTap: () => navigateToCreateCommunity(context),
+  //           ),
+  //           ref.watch(userCommunitiesProvider).when(
+  //                 data: (communities) => Expanded(
+  //                   child: ListView.builder(
+  //                     itemCount: communities.length,
+  //                     itemBuilder: (BuildContext context, int index) {
+  //                       final community = communities[index];
+  //                       return ListTile(
+  //                         leading: CircleAvatar(
+  //                           backgroundImage: NetworkImage(community.avatar),
+  //                         ),
+  //                         title: Text('r/${community.name}'),
+  //                         onTap: () {},
+  //                       );
+  //                     },
+  //                   ),
+  //                 ),
+  //                 error: (error, stackTrace) => ErrorText(
+  //                   error: error.toString(),
+  //                 ),
+  //                 loading: () => const Loader(),
+  //               ),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final user = ref.watch(userProvider)!;
+    final isGuest = !user.isAuthenticated;
+
     return Drawer(
       child: SafeArea(
         child: Column(
           children: [
-            ListTile(
-              title: const Text('Create a Community'),
-              leading: const Icon(Icons.add),
-              onTap: () => navigateToCreateCommunity(context),
-            ),
-            ref.watch(userCommunitiesProvider).when(
-                  data: (communities) => Expanded(
-                    child: ListView.builder(
-                      itemCount: communities.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        final community = communities[index];
-                        return ListTile(
-                          leading: CircleAvatar(
-                            backgroundImage: NetworkImage(community.avatar),
-                          ),
-                          title: Text('r/${community.name}'),
-                          onTap: () {},
-                        );
-                      },
+            isGuest
+                ? const SignInButton()
+                : ListTile(
+                    title: const Text('Create a community'),
+                    leading: const Icon(Icons.add),
+                    onTap: () => navigateToCreateCommunity(context),
+                  ),
+            if (!isGuest)
+              ref.watch(userCommunitiesProvider).when(
+                    data: (communities) => Expanded(
+                      child: ListView.builder(
+                        itemCount: communities.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          final community = communities[index];
+                          return ListTile(
+                            leading: CircleAvatar(
+                              backgroundImage: NetworkImage(community.avatar),
+                            ),
+                            title: Text('r/${community.name}'),
+                            onTap: () {
+                              navigateToCommunity(context, community);
+                            },
+                          );
+                        },
+                      ),
                     ),
+                    error: (error, stackTrace) => ErrorText(
+                      error: error.toString(),
+                    ),
+                    loading: () => const Loader(),
                   ),
-                  error: (error, stackTrace) => ErrorText(
-                    error: error.toString(),
-                  ),
-                  loading: () => const Loader(),
-                ),
           ],
         ),
       ),

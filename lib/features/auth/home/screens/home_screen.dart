b4f5e85f51eb05c318d0722a762a122c7a1e5f22
Chizaram Chibueze <chizy7@gmail.com>
2022-12-08 21:33:03 -0500

    Logout implemented and profile drawer

--- lib/features/auth/home/screens/home_screen.dart
+++ lib/features/auth/home/screens/home_screen.dart
@@ -3,12 +3,17 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_clone/features/auth/controller/auth_controller.dart';
 import 'package:reddit_clone/features/auth/home/delegates/search_community_delegate.dart';
 import 'package:reddit_clone/features/auth/home/drawers/community_list_drawer.dart';
+import 'package:reddit_clone/features/auth/home/drawers/profile_drawer.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({Key? Key}) : super(key: Key);
 
   void displayDrawer(BuildContext context) {
     Scaffold.of(context).openDrawer();
+  }
+
+  void displayEndDrawer(BuildContext context) {
+    Scaffold.of(context).openEndDrawer();
   }
 
   @override
@@ -32,15 +37,18 @@ class HomeScreen extends ConsumerWidget {
             },
             icon: const Icon(Icons.search),
           ),
-          IconButton(
-            icon: CircleAvatar(
-              backgroundImage: NetworkImage(user.profilePic),
-            ),
-            onPressed: () {},
-          ),
+          Builder(builder: (context) {
+            return IconButton(
+              icon: CircleAvatar(
+                backgroundImage: NetworkImage(user.profilePic),
+              ),
+              onPressed: () => displayEndDrawer(context),
+            );
+          }),
         ],
       ),
-      drawer: CommunityListDrawer(),
+      drawer: const CommunityListDrawer(),
+      endDrawer: const ProfileDrawer(),
     );
   }
 }
--- lib/features/community/screens/create_community_screen.dart
+++ lib/features/community/screens/create_community_screen.dart
@@ -27,56 +27,6 @@ class _CreateCommunityScreenState extends ConsumerState<CreateCommunityScreen> {
         .createCommunity(communityNameController.text.trim(), context);
   }
 
-  // @override
-  // Widget build(BuildContext context) {
-  //   final isLoading = ref.watch(communityControllerProvider);
-  //   return Scaffold(
-  //     appBar: AppBar(
-  //       title: const Text('Create a community'),
-  //     ),
-  //     body: isLoading
-  //         ? const Loader()
-  //         : Padding(
-  //             padding: const EdgeInsets.all(10.0),
-  //             child: Column(
-  //               children: [
-  //                 const Align(
-  //                   alignment: Alignment.topLeft,
-  //                   child: Text('Community name'),
-  //                 ),
-  //                 const SizedBox(height: 10),
-  //                 TextField(
-  //                   controller: communityNameController,
-  //                   decoration: const InputDecoration(
-  //                     hintText: 'r/Community_name',
-  //                     filled: true,
-  //                     border: InputBorder.none,
-  //                     contentPadding: EdgeInsets.all(18),
-  //                   ),
-  //                   maxLength: 21,
-  //                 ),
-  //                 const SizedBox(
-  //                   height: 30,
-  //                 ),
-  //                 ElevatedButton(
-  //                   onPressed: createCommunity,
-  //                   style: ElevatedButton.styleFrom(
-  //                     minimumSize: const Size(double.infinity, 50),
-  //                     shape: RoundedRectangleBorder(
-  //                       borderRadius: BorderRadius.circular(20),
-  //                     ),
-  //                   ),
-  //                   child: const Text(
-  //                     'Create community',
-  //                     style: TextStyle(fontSize: 17),
-  //                   ),
-  //                 ),
-  //               ],
-  //             ),
-  //           ),
-  //   );
-  // }
-
   @override
   Widget build(BuildContext context) {
     final isLoading = ref.watch(communityControllerProvider);

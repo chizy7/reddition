77201996d91f47fa4a9c2c06c6bfe264ad31fcd6
Chizaram Chibueze <chizy7@gmail.com>
2022-12-08 23:05:51 -0500

    Moderator tools implemented

--- lib/router.dart
+++ lib/router.dart
@@ -3,6 +3,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:reddit_clone/features/auth/screens/login_screen.dart';
+import 'package:reddit_clone/features/community/screens/add_mods_screen.dart';
 import 'package:reddit_clone/features/community/screens/community_screen.dart';
 import 'package:reddit_clone/features/community/screens/create_community_screen.dart';
 import 'package:reddit_clone/features/community/screens/edit_community_screen.dart';
@@ -31,6 +32,11 @@ final loggedInRoute = RouteMap(routes: {
       ),
   '/edit-community/:name': (routeData) => MaterialPage(
         child: EditCommunityScreen(
+          name: routeData.pathParameters['name']!,
+        ),
+      ),
+  '/add-mods/:name': (routeData) => MaterialPage(
+        child: AddModsScreen(
           name: routeData.pathParameters['name']!,
         ),
       ),
